d8697495d876d342bda153ca606b29694c34b2e3
Chris Chase <cchase@redhat.com>
2021-10-25 09:32:30 -0400

    Updated consumer to skip old messages.

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ KAFKA_SECURITY_PROTOCOL = os.getenv('KAFKA_SECURITY_PROTOCOL')
 KAFKA_SASL_MECHANISM = os.getenv('KAFKA_SASL_MECHANISM')
 KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
 KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
-KAFKA_CONSUMER_GROUP = 'object-detection-consumer-group'
+KAFKA_CONSUMER_GROUP = 'dog-detector-consumer-group'
 KAFKA_CONSUMER_TOPIC = os.getenv('KAFKA_TOPIC_IMAGES')
 KAFKA_PRODUCER_TOPIC = os.getenv('KAFKA_TOPIC_OBJECTS')
 
 
 def main():
+    # Normally, we'd never want to lose a message,
+    # but we want to ignore old messages for this demo, so we set
+    # enable_auto_commit=False
+    # auto_offset_reset='latest' (Default)
+    # This has the effect of starting from the last message.
+
     consumer = KafkaConsumer(KAFKA_CONSUMER_TOPIC,
                              group_id=KAFKA_CONSUMER_GROUP,
                              bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
@@ -21,7 +27,7 @@ def main():
                              sasl_mechanism=KAFKA_SASL_MECHANISM,
                              sasl_plain_username=KAFKA_USERNAME,
                              sasl_plain_password=KAFKA_PASSWORD,
-                             auto_offset_reset='earliest',
+                             enable_auto_commit=False,
                              api_version_auto_timeout_ms=30000,
                              request_timeout_ms=450000)
 
